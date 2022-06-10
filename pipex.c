d01b22dd709c19ec4f3d64348a5202a7144eb65d
genouf <gabriel.enouf@yahoo.fr>
2022-06-10 22:39:41 +0200

    decouverte du projet

--- pipex.c
+++ pipex.c
@@ -6,7 +6,54 @@
 /*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 17:09:10 by genouf            #+#    #+#             */
-/*   Updated: 2022/06/10 17:09:25 by genouf           ###   ########.fr       */
+/*   Updated: 2022/06/10 22:36:38 by genouf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "pipex.h"
+
+int	main(int argc, char **argv)
+{
+	int	pfd[2];
+	int	pid;
+	char *args_l[] = {"ls", (char *)0};
+	char *env_args_l[] = { (char *)0};
+	char *args_w[] = {"wc", (char *)0};
+	char *env_args_w[] = { (char *)0};
+	
+
+	(void)argc;
+	(void)argv;
+	/* init du pipe */
+	if (pipe(pfd) == -1)
+	{
+		ft_printf("pipe failed\n");
+		return (1);
+	}
+	/* init du child */
+	if ((pid = fork()) < 0)
+	{
+		ft_printf("fork failed\n");
+		return (2);
+	}
+	/* process du child */
+	if (pid == 0)
+	{
+		close(pfd[1]);
+		dup2(pfd[0], 0);
+		close(pfd[0]);
+		execve(args_w[0], args_w, env_args_w);
+		ft_printf("wc failed");
+		return (3);
+	}
+	else
+	{
+		close(pfd[0]);
+		dup2(pfd[1], 1);
+		close(pfd[1]);
+		execve(args_l[0], args_l, env_args_l);
+		ft_printf("ls failed");
+		return (4);
+	}
+	return (0);
+}
--- pipex.h
+++ pipex.h
@@ -6,7 +6,18 @@
 /*   By: genouf <genouf@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/10 17:09:13 by genouf            #+#    #+#             */
-/*   Updated: 2022/06/10 17:09:14 by genouf           ###   ########.fr       */
+/*   Updated: 2022/06/10 21:17:03 by genouf           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#ifndef PIPEX_H
+# define PIPEX_H
+
+/*		INCLUDE		*/
+#include <memory.h>
+#include <unistd.h>
+#include "libft/libft.h"
+
+/*		PROTOTYPE		*/
+
+#endif
